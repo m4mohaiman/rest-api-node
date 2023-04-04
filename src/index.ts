e7f035f5dab226f4ab184364bcc330a4766aa70f
batman <mohaiman.abdul@intellier.com>
2023-04-04 11:03:04 +0600

    mongo connect and env add

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose from 'mongoose';
+require('dotenv').config();
 
 const app = express();
 
@@ -20,3 +22,9 @@ const server = http.createServer(app);
 server.listen(6969, ()=> {
     console.log('Server is running on http://localhost:6969/');
 })
+
+//mongodb connect
+const MONGO_URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.saytryj.mongodb.net/?retryWrites=true&w=majority`
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error : Error) => console.log(error));
